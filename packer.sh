347546d5292bf62d2af2a9af134c6298e4d74da3
Derek Buckley <dbuckley@fed-ito-lt01.bcd.prod.bcdmetal.com>
2018-11-19 14:34:37 -0500

    Added dynamic checksum of iso to packer.sh

--- packer.sh
+++ packer.sh
@@ -1,7 +1,8 @@
 cd $(dirname "$0")
 
 image="$(echo $1 | cut -d '.' -f1)"
-packer build "$image.json"
+checksum="$(md5sum iso/$image.iso | cut -d ' ' -f1)"
+packer build -var "iso_checksum=$checksum" "$image.json"
 mkdir -p "images/$image"
 mv output-qemu/packer-qemu "images/$image/$image.qemu"
 rm -rf output-qemu

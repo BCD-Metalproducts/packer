dce1858a43492d428e6192a97a4c4d5d3c3bb203
Derek Buckley <dbuckley@fed-ito-lt01.bcd.prod.bcdmetal.com>
2018-11-20 08:46:48 -0500

    Fixed packer.sh where it was saving images with .qemu

--- packer.sh
+++ packer.sh
@@ -6,5 +6,5 @@ image="$(echo $1 | cut -d '.' -f1)"
 checksum="$(md5sum iso/$image.iso | cut -d ' ' -f1)"
 packer build -var "iso_checksum=$checksum" "$image.json"
 mkdir -p "images/$image"
-mv output-qemu/packer-qemu "images/$image.qemu"
+mv output-qemu/packer-qemu "images/$image.qcow"
 rm -rf output-qemu
